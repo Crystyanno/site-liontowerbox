5db457efe62f53271643e2a9d266c4d2c899d9bc
Crystyanno Souza <crystyanno.alves@gmail.com>
2024-11-07 21:21:54 -0300

    add equipament and wifi

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,38 @@
-//Script File
+// Script File
 
+// Home Section Starts
 var hamburgerBtn = document.querySelector('.main-navbar .hamburger-btn');
-hamburgerBtn.addEventListener( 'click', activeClass);
+var navList = document.querySelector('.main-navbar .nav-list');
+var navListItems = document.querySelectorAll('.nav-list li a');
+
+hamburgerBtn.addEventListener('click', activeClass);
 
 function activeClass(){
-    hamburgerBtn.classList.toggle('active')
+	hamburgerBtn.classList.toggle('active');
+	navList.classList.toggle('active');
 }
+
+for(var i = 0; i < navListItems.length; i++){
+	navListItems[i].addEventListener('click', listItemClicked);
+}
+
+function listItemClicked(){
+	hamburgerBtn.classList.remove('active');
+	navList.classList.remove('active');
+}
+
+// Code For Navbar
+var homeSection = document.querySelector('#home');
+window.addEventListener('scroll', pageScrollFunction);
+window.addEventListener('load', pageScrollFunction);
+
+function pageScrollFunction(){
+	if(window.scrollY > 150){
+		homeSection.classList.add('active');
+	}
+	else{
+		homeSection.classList.remove('active');
+	}
+}
+
+// Home Section Ends
